35dd2447d67d42adb3b38bc9c7f3440419402ae2
Pablo Luan <pablo-544@hotmail.com>
2025-07-13 15:36:11 -0300

    refactor(auth): clean up login controller and service by removing unnecessary logs

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -8,18 +8,15 @@ export class AuthController {
   static async login(request: FastifyRequest, reply: FastifyReply) {
 
     const loginSchema = z.object({
-        id: z.string().uuid(),
+      id: z.string().uuid(),
     })
 
     const { id } = loginSchema.parse(request.body)
-    
+
     try {
-        const signJwt = request.server.jwt.sign
-        console.log("to aqui")
-        const result = await authService.login(id, signJwt)
-        console.log('Login successful:', result)
+      const signJwt = request.server.jwt.sign
+      const result = await authService.login(id, signJwt)
       return reply.send(result)
-        
     } catch (error) {
       console.log('Login error:', error)
       return reply.status(500).send({ error: 'Internal Server Error' })
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -4,21 +4,13 @@ import { JwtSignFunction } from '../types/jwt-types'
 
 export class AuthService {
   async login(id: string, signJwt: JwtSignFunction) {
-    console.log(id)
-
-    console.log("erro no service")
-
     const user = await prisma.user.findUnique({
       where: { id },
     })
-
-    console.log('User found:', user)
     if (!user) {
       throw new Error('Usuario não encontrado')
     }
-
     const token = generateToken(signJwt, user)
-
     return { token }
   }
 }
