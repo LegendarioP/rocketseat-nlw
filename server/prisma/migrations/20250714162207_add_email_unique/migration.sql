4d337638950aadc4b6f8aebe84b0c1338699ca7e
Pablo Luan <pablo-544@hotmail.com>
2025-07-14 13:23:02 -0300

    feat(db): add new User table with email, password, and updatedAt fields, and create unique indexes for email and githubId

---
+++ server/prisma/migrations/20250714162207_add_email_unique/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "githubId" INTEGER,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatarUrl" TEXT,
+    "color" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Memory" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "coverUrl" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Memory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
