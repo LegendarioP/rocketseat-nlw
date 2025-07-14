cc3458d893af99f6ba5ecc0a439d02a6fb068fe6
Pablo Luan <pablo-544@hotmail.com>
2025-07-14 20:26:32 -0300

    feat(user): add update user functionality with email uniqueness check

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,5 +1,5 @@
 
-import { CreateUserDTO } from "../dtos/user.dto";
+import { CreateUserDTO, UpdateUserDTO } from "../dtos/user.dto";
 import { UserAlreadyExistsError, UserNotFoundError } from "../errors/user-error";
 import { prisma } from "../utils/prisma";
 import { generateRandomHexColor } from '../utils/color';
@@ -35,6 +35,32 @@ export class UserService {
         const { password, ...userWithoutPassword } = user;
         return userWithoutPassword;
     }
+
+    async update(id: string, data: UpdateUserDTO) {
+        let user = await prisma.user.findUnique({
+            where: { id }
+        })
+        if (!user) {
+            throw new UserNotFoundError();
+        }
+
+        if (data.email && data.email !== user.email) {
+            const existingUser = await prisma.user.findUnique({
+                where: { email: data.email }
+            });
+            if (existingUser) {
+                throw new UserAlreadyExistsError("Email already in use");
+            }
+        }
+
+        user = await prisma.user.update({
+            where: { id },
+            data
+        });
+
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword
+    }                    
 
     async list(){
         const users = await prisma.user.findMany({
